362a60b6fbc26eff7298ba42e0313e172a0305b8
wirrch <wgb4@kent.ac.uk>
2024-11-27 11:53:53 +0000

    Completed

--- email.py
+++ email.py
@@ -1,1 +1,10 @@
+import re
 
+def main():
+    email = str(input("Enter an email address: "))
+    if re.search(r"\w+@\w.+\.(ac.uk|gov.uk|nhs.net)", email):
+        print("Valid")
+    else:
+        print("Invalid")
+
+main()
--- postcode.py
+++ postcode.py
@@ -1,1 +1,12 @@
+import re
 
+def main():
+    address = "University of Kent, Canterbury, Kent, CT2 7NT"
+    postcode = address.split(", ")[3]
+    print(postcode)
+    if re.search(r"^\w{2,3}\s{1}\w{2,3}$", postcode):
+        print("Valid")
+    else:
+        print("Invalid")
+
+main()
